de62cccf4a88e7314345cd1a1c878641cbdd226e
N210013 - Karthik <n210013@rguktn.ac.in>
2025-02-04 13:56:46 +0530

    Updated admin page

--- alumni/admin.py
+++ alumni/admin.py
@@ -3,6 +3,10 @@ from . import models
 
 # Register your models here.
 
+admin.site.site_header = "Alumni Portal Admin"
+admin.site.site_title = "Alumni Admin"
+admin.site.index_title = "Welcome to the Alumni Management System"
+
 admin.site.register(models.NewsPage)
 admin.site.register(models.Achievement)
 admin.site.register(models.Alumni)
