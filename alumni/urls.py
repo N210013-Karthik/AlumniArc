547ed1f881789b937d8877b41e18c1aede78c0bb
N210013 - Karthik <n210013@rguktn.ac.in>
2025-02-04 13:12:03 +0530

    included navbar and footer

--- alumni/urls.py
+++ alumni/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage_view, name='homepage'),
-    path('login/', views.login_view, name='login'),
     path('news/', views.news_list, name='news_list'),
     path('achievements/', views.achievements_list, name='achievements_list'),
     path('alumni/', views.alumni_list, name='alumni_list'),
